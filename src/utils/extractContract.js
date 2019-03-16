c201eb3efa9bad8a260a5e9f91e481bd2a0dfa30
RobertoC27 <chiroy_134@hotmail.com>
2019-03-16 12:52:21 -0600

    removed unnecesary files

--- src/utils/extractContract.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-
-var file = fs.readFileSync('../contracts/Test.json');
-var content = JSON.parse(file);
-console.log(content.abi);
-fs.writeFileSync('prueba.json', content.abi.toString());
-console.log(__dirname);
-
